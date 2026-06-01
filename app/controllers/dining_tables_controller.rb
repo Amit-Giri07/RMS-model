1f92339c6a4bdaf2672d702b398b334c4e1e4a7c
Amit-Giri07 <amitgiri20620316@gmail.com>
2026-06-01 19:17:23 +0545

    Implement CRUD operations

---
+++ app/controllers/dining_tables_controller.rb
@@ -0,0 +1,2 @@
+class DiningTablesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :restaurants
+  resources :dining_tables
+  resources :menu_items
+
   get "dashboard/index"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
