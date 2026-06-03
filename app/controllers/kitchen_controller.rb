68e7865e65a3eca43c4e49390c948ffd4f55a006
Amit-Giri07 <amitgiri20620316@gmail.com>
2026-06-03 12:22:21 +0545

    Add order status workflow

--- app/controllers/kitchen_controller.rb
+++ app/controllers/kitchen_controller.rb
@@ -4,4 +4,14 @@ class KitchenController < ApplicationController
       status: [ :pending, :preparing ]
     )
   end
+
+  def update
+    @order = Order.find(params[:id])
+
+    @order.update(
+      status: params[:order][:status]
+    )
+
+  redirect_back fallback_location: orders_path
+  end
 end
